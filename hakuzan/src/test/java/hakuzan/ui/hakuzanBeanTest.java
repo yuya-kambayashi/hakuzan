d33236e0de3f2277c7aba3d3e9fdf41256fcd9ed
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2024-03-08 14:31:47 +0900

    ユニットテストの大枠を追加します

---
+++ hakuzan/src/test/java/hakuzan/ui/hakuzanBeanTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hakuzan.ui;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author kamba
+ */
+public class hakuzanBeanTest {
+    
+    
+    @Test
+    void Case1(){
+        
+        var actual = true;
+        var expected = true;
+        
+        assertThat(actual).isEqualTo(expected);
+
+    }
+}
